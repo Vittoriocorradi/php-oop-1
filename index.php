d9b642ef370657153c6c4eb21dcdae4b33fffb97
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-04-14 15:56:59 +0200

    Aggiunti film

--- index.php
+++ index.php
@@ -5,5 +5,13 @@ class Movie {
 }
 
 $drive = new Movie();
-$drive->title = 'drive';
+$drive->title = 'Drive';
 var_dump($drive);
+$annihilation = new Movie();
+$annihilation->title = 'Annihilation';
+var_dump($annihilation);
+var_dump($annihilation->title);
+$mulholland_drive = new Movie();
+$mulholland_drive -> title = 'Mulholland Drive';
+var_dump($mulholland_drive);
+var_dump($mulholland_drive -> title);
