e47790982df520a4011d11661bbbae0fba43016d
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-04-14 18:59:42 +0200

    Bonus 2 fix - ciclo lista

--- esercizio-principale/index.php
+++ esercizio-principale/index.php
@@ -18,60 +18,21 @@ include_once __DIR__ . '/partials/header.php';
     <!-- Lista Film -->
     <div class="col-5 mx-auto">
         <ul class="list-group">
-            <!-- Primo film -->
-            <li class="list-group-item">
-                <!-- Titolo film -->
-                <h2><?php echo $drive->title; ?></h2>
-                <!-- Lunghezza -->
-                <small>Lenght: <?php echo $drive->getLength() ?></small>
-                <div class="d-flex">
-                    <div class="me-1">Generi:</div>
-                    <?php foreach ($drive->genres as $genre) { ?>
-                        <div class="me-2"><?php echo $genre; ?></div>
-                    <?php } ?>
-            </li>
-            <!-- Secondo film -->
-            <li class="list-group-item">
-                <!-- Titolo film -->
-                <h2><?php echo $annihilation->title; ?></h2>
-                <!-- Lunghezza -->
-                <small>Lenght: <?php echo $annihilation->getLength() ?></small>
-                <!-- Generi -->
-                <div class="d-flex">
-                    <div class="me-1">Generi:</div>
-                    <?php foreach ($annihilation->genres as $genre) { ?>
-                        <div class="me-2"><?php echo $genre; ?></div>
-                    <?php } ?>
-                </div>
-            </li>
-            <!-- Terzo film -->
-            <li class="list-group-item">
-                <!-- Titolo film -->
-                <h2><?php echo $mulholland_drive->title; ?></h2>
-                <!-- Lunghezza -->
-                <small>Lenght: <?php echo $mulholland_drive->getLength() ?></small>
-                <!-- Generi -->
-                <div class="d-flex">
-                    <div class="me-1">Generi:</div>
-                    <?php foreach ($mulholland_drive->genres as $genre) { ?>
-                        <div class="me-2"><?php echo $genre; ?></div>
-                    <?php } ?>
-                </div>
-            </li>
-            <!-- Quarto film -->
-            <li class="list-group-item">
-                <!-- Titolo film -->
-                <h2><?php echo $hotel_chevalier->title; ?></h2>
-                <!-- Lunghezza -->
-                <small>Lenght: <?php echo $hotel_chevalier->getLength() ?></small>
-                <!-- Generi -->
-                <div class="d-flex">
-                    <div class="me-1">Generi:</div>
-                    <?php foreach ($hotel_chevalier->genres as $genre) { ?>
-                        <div class="me-2"><?php echo $genre; ?></div>
-                    <?php } ?>
-                </div>
-            </li>
+            <!-- Ciclo film -->
+            <?php foreach($movies as $movie) { ?>
+                <li class="list-group-item">
+                    <!-- Titolo film -->
+                    <h2><?php echo $movie->title; ?></h2>
+                    <!-- Lunghezza -->
+                    <small>Lenght: <?php echo $movie->getLength() ?></small>
+                    <div class="d-flex">
+                        <div class="me-1">Generi:</div>
+                        <?php foreach ($movie->genres as $genre) { ?>
+                            <div class="me-2"><?php echo $genre; ?></div>
+                        <?php } ?>
+                    </div>
+                </li>
+            <?php } ?>
         </ul>
         <!-- /Lista film -->
     </div>
